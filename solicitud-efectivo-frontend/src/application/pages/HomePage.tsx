9ab5e9c342a9cff6698757ed6d4be9f2703b4fda
Frandy Jeffry Cepeda <Frandycepeda1906@gmail.com>
2025-08-29 11:53:30 -0700

    Update - Test

--- solicitud-efectivo-frontend/src/application/pages/HomePage.tsx
+++ solicitud-efectivo-frontend/src/application/pages/HomePage.tsx
@@ -15,43 +15,48 @@ const Home = () => {
 
     <>
 
-        <Container className="flex justify-between">
+      <Container className="flex justify-between">
 
-         
+        <Container>
+          
+          <Container>
+
             <Title text="Listado de Solicitudes --- estt"/>
 
             <Title text="Gestiona y visualiza todas las solicitudes de efectivo" isTitle={false}/>
 
           </Container>
 
-          <Container>
-            <Link to="/new-request" className="flex items-center gap-2 text-sm sm:text-base bg-gradient-to-r from-green-600 to-emerald-600 text-white font-bold px-4 py-2 rounded-lg cursor-pointer  ">
-              <span>
-                <FaPlus size={18}/>
-              </span>
-              Nueva Solicitud
-            </Link>
-          </Container>
-
         </Container>
 
-        <section>
+        <Container>
+          <Link to="/new-request" className="flex items-center gap-2 text-sm sm:text-base bg-gradient-to-r from-green-600 to-emerald-600 text-white font-bold px-4 py-2 rounded-lg cursor-pointer  ">
+            <span>
+              <FaPlus size={18}/>
+            </span>
+            Nueva Solicitud
+          </Link>
+        </Container>
 
-          <CardSection1/>
+      </Container>
 
-        </section>
+      <section>
 
-        <section>
+        <CardSection1/>
 
-          <CardSection2/>
-          
-        </section>
+      </section>
 
-        <section>
-          
-          <RequestTable/>
-          
-        </section>
+      <section>
+
+        <CardSection2/>
+        
+      </section>
+
+      <section>
+        
+        <RequestTable/>
+        
+      </section>
     
     </>
 
