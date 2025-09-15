62d171e0a9c346fdcd537bafee65642d8d9bd5fb
eudexcode <eudysgs.s@gmail.com>
2025-09-15 16:37:52 -0400

    feat: agg el campo tipo pago en el post de solicitud

--- solicitud-efectivo-backend/src/core/domain/entities/solicitud-efectivo.entity.ts
+++ solicitud-efectivo-backend/src/core/domain/entities/solicitud-efectivo.entity.ts
@@ -16,6 +16,9 @@ export class SolicitudEfectivoEntity {
   @Column({ name: 'tipo_solicitud_id'})
   tipoSolicitudId: number;
 
+  @Column({ name: 'tipo_pago_id'})
+  tipoPagoId: number;
+
   @Column({ name: 'division_id'})
   divisionId: number;
 
